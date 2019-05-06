1155a9284a82543698dff3a1497e747f64d68b22
dan-mannion <danjohnmannion@gmail.com>
2019-05-06 14:46:01 +0100

    Corrects error in setting of idle level. Inlcuded an extra new line.

--- enterBurstMode.m
+++ enterBurstMode.m
@@ -2,7 +2,7 @@ function enterBurstMode(dg, channel, num_cycles, idle_level, trig_source)
 % ENTERBURSTMODE Puts the function gen into burst mode.
 writeCommand(dg,sprintf(':SOURCE%d:BURST:MODE TRIG', channel));
 writeCommand(dg,sprintf(':SOURCE%d:BURST:NCYCLES %d', channel , num_cycles));
-writeCommand(dg,sprintf(':SOURCE%d:BURST:IDLE %s\n', channel , idle_level));
+writeCommand(dg,sprintf(':SOURCE%d:BURST:IDLE %s', channel , idle_level));
 writeCommand(dg,sprintf(':SOURCE%d:BURST:TRIG:SOURCE %s', channel , trig_source));
 writeCommand(dg,sprintf(':SOURCE%d:BURST:STATE 1', channel));
 end
