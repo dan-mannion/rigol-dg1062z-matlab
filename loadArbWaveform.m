5b7171004c813e27f0b6fa5cb39d710de52a318a
dan-mannion <danjohnmannion@gmail.com>
2019-05-06 10:01:02 +0100

    Adds check for extended memory option expanding to 16000 data points. Also adds parameter to change the channel.

--- loadArbWaveform.m
+++ loadArbWaveform.m
@@ -1,8 +1,21 @@
-function loadArbWaveform(dg, fs, wave)
-cmd = sprintf(':SOURCE1:APPLY:ARB %1.3e, 2, 0',fs);
+function loadArbWaveform(dg, channel, fs, wave, extended_mem)
+
+if(~exists('extended_mem','var'))
+    if(length(wave)>8000)
+        ME = MException('loadArbWaveform:WaveTooLong','Wave must not exceed 8000 points.');
+        throw(ME);
+    end
+else
+    if(length(wave)>16000)
+        ME = MException('loadArbWaveform:WaveTooLong','Wave must not exceed 16000 points.');
+        throw(ME);
+    end
+end
+
+cmd = sprintf(':SOURCE%d:APPLY:ARB %1.3e, 2, 0',channel, fs);
 writeCommand(dg, cmd);
 sig_str = sprintf(',%1.2e',wave);
-cmd = sprintf(':SOURCE1:DATA VOLATILE%s', sig_str); 
+cmd = sprintf(':SOURCE%d:DATA VOLATILE%s', channel, sig_str); 
 writeCommand(dg, cmd);
 clear sig_str;
 
