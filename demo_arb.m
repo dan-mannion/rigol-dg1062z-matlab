f58cde8f4a8024ff08db1f283873f521f39d51d6
dan-mannion <danjohnmannion@gmail.com>
2019-05-06 13:50:27 +0100

    Removes excess code from the demo. This now demosntrates how to easily upload a arb waveform.

--- demo_arb.m
+++ demo_arb.m
@@ -7,23 +7,4 @@ writeCommand(dg, ':SOUR1:FUNC:ARB:MODE SRATE');
 writeCommand(dg, sprintf(':SOUR1:FUNC:ARB:SRATE %1.3e', fs));
 
 loadArbWaveform(dg, 1, fs, v);
-
-
-% for x = 1:length(v)
-%    cmd = sprintf(':SOURCE1:TRACE:DATA:VALUE VOLATILE,%d,%1.3e', x, v(x)); 
-%    writeCommand(dg, cmd);
-% end
-
-
-% sig_str = sprintf(',%1.1f',v);
-% cmd = sprintf(':SOURCE1:DATA VOLATILE%s', sig_str); 
-% writeCommand(dg, cmd);
-% cmd = sprintf(':SOURCE1:APPLY:ARB 1000, 2, 0');
-% fprintf(dg, cmd);
-% writeCommand(dg, cmd);
-% sig_str = sprintf(',%1.3f',v);
-% cmd = sprintf(':SOURCE1:DATA VOLATILE%s', sig_str); 
-% writeCommand(dg, cmd);
-% fprintf(dg, ':OUTPUT1 ON\n');
-% clear sig_str;
 dgDisconnect(dg);
