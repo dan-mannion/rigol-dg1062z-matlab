644111c388b741280a9ba17651834991ad58589c
dan-mannion <danjohnmannion@gmail.com>
2019-05-06 13:49:37 +0100

    Removed excess code. Currently implementation is best it can be. Errors will occur when large commands are sent. Such as a long arb waveform.

--- writeCommand.m
+++ writeCommand.m
@@ -4,7 +4,7 @@ function writeCommand(dg, str)
 %Then the command is split into 512 sized chunks and seent separately. 
 
 if(length(str)>dg.OutputBufferSize)
-      dg.OutputBufferSize=length(str)+1;
+      dg.OutputBufferSize=length(str)+2;
 end
 fopen(dg);
 dg.EOSMode = 'write';
@@ -12,15 +12,4 @@ fprintf(dg, '%s\n',str);
 dg.EOSMode = 'none';
 fclose(dg);
 dg.OutputBufferSize=512;
-
-% N = floor(length(str)/512);
-% dg.EOSMode = 'none';
-% fopen(dg);
-% for n=1:N
-%    fprintf(dg,'%s', str((n-1)*512+1:n*512)); 
-% end
-% %Write remainder
-% dg.EOSMode = 'write';
-% fprintf(dg,'%s\n',str(N*512+1:end));
-% fclose(dg);
 end
